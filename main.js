791b868372528bcf5007e9d1cfc35ec66efc2014
mangster80 <magnus.wallin@student.kyh.se>
2018-03-15 09:51:25 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
+
+// let student = {
+//   firstname: “Lisa”,
+//   lastname: “LaFlamme”,
+//   age: 21,
+//   classes: [“street”, “dancehall”, “balett”]
+// };
+//
+// let teacher = {
+//   firstname: “Adrienne”,
+//   lastname: “Picard”,
+//   age: 28,
+//   classes: [“street”, “dancehall”, “balett”]
+// };
+//
+// let classes = [{
+//   name: “street”,
+//   dates: [“2018-03-16”, “2018-03-23”, “2018-03-30”],
+//   start: “18.00”,
+//   length: 2,
+// },
+
+// $(function () {
+//   $('#datetimepicker2').datetimepicker(){
+//   locale: 'eu'
+//   });
+// });
+//
+// // Data Picker Initialization
+// $('.datepicker').pickadate();
