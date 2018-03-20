0308e8e2345522b7dd6ebf5cadc2ec44dac6a91a
Alexosk <alexsigurdar@gmail.com>
2018-03-20 13:44:32 +0100

    Fix login problem

--- js/index.js
+++ js/index.js
@@ -20,94 +20,94 @@ app.use(function(req, res, next) {
 app.listen(port);
 
 const student = [{
-      "firstname":"Lisa",
-      "lastname":"LaFlamme",
-      "age":21,
-      "classes":["street", "dancehall", "balett"]
+      firstname:"Lisa",
+      lastname:"LaFlamme",
+      age:21,
+      classes:["street", "dancehall", "balett"]
     },
     {
-      "firstname": "Alex",
-      "lastname": "Pitt",
-      "age": 32,
-      "classes": ["street", "balett"]
+      firstname: "Alex",
+      lastname: "Pitt",
+      age: 32,
+      classes: ["street", "balett"]
     },
     {
-      "firstname": "Mange",
-      "lastname": "Kardashian",
-      "age": 35,
-      "classes": ["street"]
+      firstname: "Mange",
+      lastname: "Kardashian",
+      age: 35,
+      classes: ["street"]
     },
     {
-      "firstname": "Jonny",
-      "lastname": "Wallin",
-      "age": 19,
-      "classes": ["street", "dancehall", "balett"]
+      firstname: "Jonny",
+      lastname: "Wallin",
+      age: 19,
+      classes: ["street", "dancehall", "balett"]
     },
     {
-      "firstname": "Filip",
-      "lastname": "Bentzer",
-      "age": 17,
-      "classes": ["street", "dancehall", "balett"]
+      firstname: "Filip",
+      lastname: "Bentzer",
+      age: 17,
+      classes: ["street", "dancehall", "balett"]
     },
     {
-      "firstname": "Ronja",
-      "lastname": "Rövardotter",
-      "age": 45,
-      "classes": ["street", "balett"]
+      firstname: "Ronja",
+      lastname: "Rövardotter",
+      age: 45,
+      classes: ["street", "balett"]
     },
     {
-      "firstname": "Kristian",
-      "lastname": "Rigaud",
-      "age": 39,
-      "classes": ["balett"]
+      firstname: "Kristian",
+      lastname: "Rigaud",
+      age: 39,
+      classes: ["balett"]
     },
     {
-      "firstname": "Julia",
-      "lastname": "Bessouda",
-      "age": 31,
-      "classes": ["balett", "street"]
+      firstname: "Julia",
+      lastname: "Bessouda",
+      age: 31,
+      classes: ["balett", "street"]
     },
     {
-      "firstname": "Maryam",
-      "lastname": "Kitten",
-      "age": "25",
-      "classes": ["balett", "street"]
+      firstname: "Maryam",
+      lastname: "Kitten",
+      age: "25",
+      classes: ["balett", "street"]
     },
     {
-      "firstname": "Camilla",
-      "lastname": "Gustavsson",
-      "age": 22,
-      "classes": ["balett", "street"]
+      firstname: "Camilla",
+      lastname: "Gustavsson",
+      age: 22,
+      classes: ["balett", "street"]
     },
     {
-      "firstname": "Cecilia",
-      "lastname": "Sigurdardottir",
-      "age": 21,
-      "classes": ["balett", "street", "dancehall"]
+      firstname: "Cecilia",
+      lastname: "Sigurdardottir",
+      age: 21,
+      classes: ["balett", "street", "dancehall"]
     },
     {
-      "firstname": "Frida",
-      "lastname": "Svahnen",
-      "age": 27,
-      "classes": ["balett", "street", "dancehall"]
+      firstname: "Frida",
+      lastname: "Svahnen",
+      age: 27,
+      classes: ["balett", "street", "dancehall"]
     },
     {
-      "firstname": "Mattias",
-      "lastname": "Chiquita",
-      "age": 27,
-      "classes": ["balett", "dancehall"]
+      firstname: "Mattias",
+      lastname: "Chiquita",
+      age: 27,
+      classes: ["balett", "dancehall"]
     },
     {
-      "firstname": "Leo",
-      "lastname": "Milojevic",
-      "age": 30,
-      "classes": ["balett", "dancehall"]
+      firstname: "Leo",
+      lastname: "Milojevic",
+      age: 30,
+      classes: ["balett", "dancehall"]
     },
     {
-      "firstname": "Grímúlfur",
-      "lastname": "Grímsson",
-      "age": 59,
-      "classes": ["balett", "dancehall"]
+      firstname: "Grímúlfur",
+      lastname: "Grímsson",
+      age: 59,
+      classes: ["balett", "dancehall"]
   }];
 
 
@@ -119,26 +119,35 @@ const teacher = [{
   classes: "street",
 },
   {
-  "firstname":"Rocky",
-  "lastname":"Balboa",
-  "age": 39,
-  "classes":["street", "dancehall", "balett"]
+  firstname:"Rocky",
+  lastname:"Balboa",
+  age: 39,
+  classes:["street", "dancehall", "balett"]
 },
   {
-  "firstname":"Sally",
-  "lastname":"Sjöström",
-  "age": 1,
-  "classes":["street", "dancehall", "balett"]
+  firstname:"Sally",
+  lastname:"Sjöström",
+  age: 1,
+  classes:["street", "dancehall", "balett"]
 }];
 
 const classes = [{
-  "name": "Street"
+  name: "Street",
+  dates: ["2018-03-16", "2018-03-23", "2018-03-30"],
+  start: "18:00",
+  length: 2
 },
 {
-  "name": "dancehall"
+  name: "dancehall",
+  dates: ["2018-03-17", "2018-03-24", "2018-04-01"],
+  start: "18:00",
+  length: 2
 },
 {
-  "name": "balett"
+  name: "balett",
+  dates: ["2018-03-18", "2018-03-25", "2018-04-02"],
+  start: "18:00",
+  length: 2
 }];
 
 // todo add classes
@@ -153,9 +162,11 @@ let res;
 app.get('/student', function(req, res) {
   res.json(student);
 });
-
 app.get('/teacher', function(req, res) {
   res.json(teacher);
+});
+app.get('/classes', function(req, res) {
+  res.json(classes);
 });
 
 // todo add classes endpoint
--- js/login.js
+++ js/login.js
@@ -1,29 +1,22 @@
 let login = function () {
-  let username = document.forms["form-control"]["username"].value;
-  let password = document.forms["form-control"]["password"].value;
-  if (username == "Mange" && password == "123") {
-    window.location = student.html;
+  let username = document.getElementById('username').value
+  let password = document.getElementById('psw').value
+  if (username === "Mange" && password === "123") {
+    console.log('Mange');
+    document.location.href = 'http://10.7.2.4:3000/html/student.html'
     return false;
 
   }
-  else if (username == "Alex" && password == "123") {
-    window.location = ../html/student.html;
+  else if (username === "Alex" && password === "123") {
+    document.location.href = 'http://10.7.2.4:3000/html/student.html'
+    console.log('Alex');
+  }
+  else if (username === "teacher" && password === "123"){
+    console.log('teacher');
+    document.location.href = 'http://10.7.2.4:3000/html/teacher.html'
     return false;
   }
-  else if (username == "teacher" && password == "123"){
-    window.location = ../hmtl/teacher.html;;
-    return false;
+  else {
+    alert('finns inte')
   }
-}
-
-let student = {
-  firstName: ["Magnus", "Alex"],
-  lastName: "Wallin",
-  password: pass,
-  email: mange@mail.com,
-  classes: ["street", "dancehall", "ballet"]
-};
-
-let teacher = {
-  firstName: []
 }
